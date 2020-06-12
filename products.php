5ff5e8cf1468be975d38bb3911f788a7abf9614e
aliandrafreimann <33056950+aliandrafreimann@users.noreply.github.com>
2020-06-13 01:51:38 +0300

    brr

--- products.php
+++ products.php
@@ -1,1 +1,25 @@
 <?php
+
+return [
+    1 => [
+        'id' => 1,
+        'name' => 'Product1',
+        'price' => 9.99,
+        'description' => 'Description',
+        'image' => 'images/pic1.png',
+    ],
+    2 => [
+        'id' => 2,
+        'name' => 'Product2',
+        'price' => 91.99,
+        'description' => 'Description2',
+        'image' => 'images/pic1.png',
+    ],
+    3 => [
+        'id' => 3,
+        'name' => 'Product1',
+        'price' => 3.99,
+        'description' => 'Description3',
+        'image' => 'images/pic3.png',
+    ]
+];
